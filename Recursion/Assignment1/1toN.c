4bc6dcd0eb3eafe3971210f47adf0d2b86cb9ac7
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-02-07 22:19:53 +0530

    Add files via upload

--- Recursion/Assignment1/1toN.c
+++ Recursion/Assignment1/1toN.c
@@ -1,1 +1,15 @@
-
+//Given a number N, write a program to print numbers from 1…N.
+#include<stdio.h>
+void increasing(int x,int n){
+    if(x>n) return ;
+    printf("%d\n",x);
+    increasing(x+1,n);
+    return;
+}
+int main(){
+    int n;
+    printf("Enter the number :");
+    scanf("%d",&n);
+    increasing(1,n);
+    return 0;
+}
---
+++ Recursion/Assignment1/fibonacci.c
@@ -0,0 +1,15 @@
+//Given a number N, write a program to calculate Fibonacci(N).
+#include<stdio.h>
+int fibo(int n){
+    if(n==1 ||n==2) return 1;
+     //if(n<=2) return 1;
+    return fibo(n-1)+fibo(n-2);
+}
+int main(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    //int x=fibo(n);
+    printf("%d",fibo(n));
+    return 0;
+}
